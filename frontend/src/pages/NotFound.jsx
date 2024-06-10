75de31c4f4d11aa95f6d388eedeb1ba986f3c70a
Liudvikas Bajarūnas <bajarunasmain@gmail.com>
2024-06-10 18:32:12 +0300

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>Not found</h1>
+      <p>The page that you'r looking for does not exists</p>
+    </div>
+  );
+}
+
+export default NotFound;
